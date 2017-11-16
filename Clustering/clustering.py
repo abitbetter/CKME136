a1aa5e3df91f34325f071a847a629cf2613af0fe
AlexLadd <32050620+AlexLadd@users.noreply.github.com>
2017-11-16 12:15:57 -0500

    Update clustering.py

--- Clustering/clustering.py
+++ Clustering/clustering.py
@@ -27,31 +27,10 @@ print(kmodes_cao.cluster_centroids_)
 print('Final training cost: {}'.format(kmodes_cao.cost_))
 print('Training iterations: {}'.format(kmodes_cao.n_iter_))
 
-# print('Results tables:')
-# for result in (kmodes_huang, kmodes_cao):
-#     classtable = np.zeros((4, 4), dtype=int)
-#     for ii, _ in enumerate(y):
-#         classtable[int(y[ii][-1]) - 1, result.labels_[ii]] += 1
-
-#     print("\n")
-#     print("    | Cl. 1 | Cl. 2 | Cl. 3 | Cl. 4 |")
-#     print("----|-------|-------|-------|-------|")
-#     for ii in range(4):
-#         prargs = tuple([ii + 1] + list(classtable[ii, :]))
-#         print(" D{0} |    {1:>2} |    {2:>2} |    {3:>2} |    {4:>2} |".format(*prargs))
-
-# print(x[1])
-# print(y)
-
-#this shows the resulting cluster 'label' of the restauarants(ie what cluster each restaurant belongs to)
-# print(len(kmodes_huang.labels_))
-# print(len(y))
-
 result = np.concatenate([y,kmodes_huang.labels_])
 
 #this joins the restaurant name
 result2 = np.column_stack((y,kmodes_huang.labels_))
-# print(result2)
 
 #convert numpy matrix to pandas dataframe
 result_df = pd.DataFrame(result2)
